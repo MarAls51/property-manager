d7f869b82f6cb123ae496503d137319e118eca5c
Zayne Melnick <zmelnick2021@fau.edu>
2022-11-21 09:18:32 -0500

    edit implemented properly

--- src/Components/Items/EditItems.js
+++ src/Components/Items/EditItems.js
@@ -1,19 +1,41 @@
-
 import { useUserAuth } from "../../Context/UserAuthContext";
 import { Form } from "react-bootstrap";
 import { useState } from "react";
 import back from "../../Assets/backarrow.svg";
+import { db } from "../../Context/firebase";
+import { doc, updateDoc } from "firebase/firestore";
 export const EditItems = (props) => {
   const [name, setName] = useState("");
   const [price, setPrice] = useState();
 
   const { user, setUserDataUpdated, userDataUpdated } = useUserAuth();
 
+  const handleEditItem = async (e) => {
+    e.preventDefault();
+    if (name !== "" && !isNaN(price))
+      try {
+        const query = await doc(
+          db,
+          "Users",
+          `${user.uid}`,
+          "Personal Items",
+          props.selectedItem.id
+        );
+        await updateDoc(query, {
+          Name: name,
+          Price: Number(price),
+        });
+        setUserDataUpdated(!userDataUpdated);
+      } catch (error) {
+        console.log(error.message);
+      }
+  };
+
   return (
     <>
-   <div style={{ backgroundColor: "white" }}>
-   <div className="p-3" style={{backgroundColor: "#a7a4e0"}}>
-   <button
+      <div style={{ backgroundColor: "white" }}>
+        <div className="p-3" style={{ backgroundColor: "#a7a4e0" }}>
+          <button
             style={{
               backgroundColor: "#a7a4e0",
               border: "none",
@@ -26,7 +48,7 @@ export const EditItems = (props) => {
             <img src={back} />
           </button>
         </div>
-        <Form className="p-5">
+        <Form onSubmit={handleEditItem} className="p-5">
           <Form.Group className="mb-3" controlId="formBasicName">
             <label for="formBasicName">Item Name</label>
             <Form.Control
@@ -46,13 +68,13 @@ export const EditItems = (props) => {
           </Form.Group>
 
           <div className="d-grid">
-          <button className="mb-4 btn login-btn" type="Submit">
-            Add Item
-          </button>
-        </div>
+            <button className="mb-4 btn login-btn" type="Submit">
+              Edit Item
+            </button>
+          </div>
         </Form>
       </div>
-      <div className="p-3" style={{backgroundColor: "#a7a4e0"}}/>
+      <div className="p-3" style={{ backgroundColor: "#a7a4e0" }} />
     </>
   );
 };
